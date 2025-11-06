39689082e441a53c43d8350537f0579b17c12f10
hwi-lee <riteru66@never.com>
2025-11-06 15:09:25 +0900

    test: 루아스트립트 로직 수정

--- src/main/java/com/ticketing/seat/exception/MatchNotFoundException.java
+++
@@ -1,7 +0,0 @@
-package com.ticketing.seat.exception;
-
-public class MatchNotFoundException extends RuntimeException {
-    public MatchNotFoundException(Long matchId) {
-        super("Match not found: " + matchId);
-    }
-}
